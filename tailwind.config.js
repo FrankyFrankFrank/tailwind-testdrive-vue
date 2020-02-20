ad16d6eda6b8d6f1ffaf502fd3dc155153681331
Adam Frank <a.frank@vehikl.com>
2020-02-20 09:23:53 -0500

    add component

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,30 +20,30 @@ module.exports = {
   },
   variants: {},
   plugins: [
-    // plugin(function ({ addComponents, theme }) {
-    //   const inputs = {
-    //     '.text-input': {
-    //       width: '100%',
-    //       padding: `${theme('spacing.5')} ${theme('spacing.1')}`,
-    //       textTransform: 'uppercase',
-    //       letterSpacing: theme('letterSpacing.widest'),
-    //       fontSize: theme('fontSize.lg'),
-    //       color: theme('colors.red.500'),
-    //       background: theme('colors.yellow.400'),
-    //       transition: 'all 500ms',
-    //       borderBottom: theme('borderWidth.2'),
-    //       borderColor: theme('colors.red.500'),
-    //       '&::placeholder': {
-    //         color: theme('colors.yellow.800'),
-    //       },
-    //       '&:focus': {
-    //         background: theme('colors.red.500'),
-    //         color: theme('colors.yellow.400'),
-    //       }
-    //     }
-    //   }
+    plugin(function ({ addComponents, theme }) {
+      const inputs = {
+        '.text-input': {
+          width: '100%',
+          padding: `${theme('spacing.5')} ${theme('spacing.1')}`,
+          textTransform: 'uppercase',
+          letterSpacing: theme('letterSpacing.widest'),
+          fontSize: theme('fontSize.lg'),
+          color: theme('colors.red.500'),
+          background: theme('colors.yellow.400'),
+          transition: 'all 500ms',
+          borderBottom: theme('borderWidth.2'),
+          borderColor: theme('colors.red.500'),
+          '&::placeholder': {
+            color: theme('colors.yellow.800'),
+          },
+          '&:focus': {
+            background: theme('colors.red.500'),
+            color: theme('colors.yellow.400'),
+          }
+        }
+      }
 
-    //   addComponents(inputs)
-    // })
+      addComponents(inputs)
+    })
   ],
 }
