a97855b06bd5ab774fcc918697235b84f49dbf20
Adam Frank <a.frank@vehikl.com>
2020-02-20 09:24:11 -0500

    use component class

--- src/components/Login/TextInput.vue
+++ src/components/Login/TextInput.vue
@@ -3,7 +3,7 @@
     :type="type"
     :placeholder="placeholder"
     v-on:input="$emit('input', $event.target.value)"
-    class="w-full px-1 py-5 uppercase tracking-widest text-lg bg-yellow-400 placeholder-yellow-800 focus:bg-red-500 text-red-500 focus:text-yellow-400 transition-colors border-b-2 border-red-500 duration-500"
+    class="text-input"
   />
 </template>
 
